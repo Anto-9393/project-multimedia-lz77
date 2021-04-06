ec98a2086ea27bfea33a5248096134d63485aac2
Anto-9393 <giangrecouni@gmail.com>
2021-04-06 19:25:56 +0200

    Update image.py

--- image.py
+++ image.py
@@ -9,36 +9,42 @@ def lz77Compress (image,sw,lab):
     row = img.shape[0]
     col = img.shape[1]
     ch = img.shape[2]
-    tot = img.size
+    tot = row * col * ch
     slidingWindows = sw
     lookAhead = lab 
-
-    encodedTuple = np.array([], dtype=np.uint16) 
-    encodedChar = np.array([], dtype=np.uint8)
+    print("fase 1")
+    encodedTuple = []
+    encodedChar = []
     # Lunghezza del Search Buffer
     sbSize = slidingWindows - lookAhead
+    for it in range(sbSize):
+        encodedTuple.append(0)
+        encodedChar.append (flat[it])
     # puntatore Search Buffer
     sbPointer = 0
-    while sbSize + sbPointer < tot :
+    print("fase 2")
+    while sbPointer < tot :
         max_match = 0
         max_match_go_back = 0        
         selChar = sbPointer + sbSize
      # corrispondenza del carattere in Sb da lookAd
         encodeCharacters = flat[selChar] 
     #sequenza vuota[]
-        seqY = np.array([])
+       
+        seqY = []
         for i in range(sbPointer,sbPointer + sbSize):
             if(flat[i] == encodeCharacters):
-                seqY = np.append(seqY,i)
+                seqY.append(i)
+        
     #controllare se non vi è una corrispondenza
-        if(seqY.size == 0 ):
-            encodedTuple = np.append(encodedTuple,(0,0))
-            encodedChar = np.append (encodedChar,encodeCharacters)
+        if(len(seqY) == 0 ):
+            encodedTuple.append(0)
+            encodedChar.append (encodeCharacters)
         else:
             for j in seqY:
             #lunghezza della corrispodenza
                 matchLenght= 0
-                returnBack = selChar -i
+                returnBack= selChar - j
                 it = 0 
                 while selChar + it < tot :
                     if flat[it + j] == flat[selChar + it]:
@@ -49,41 +55,40 @@ def lz77Compress (image,sw,lab):
                         break
                 if matchLenght>max_match:
                    max_match = matchLenght
-                   returnBack = max_match_go_back
+                   returnBack= max_match_go_back
 
            
-            encodedTuple = np.append(encodedTuple,(max_match_go_back,max_match))
-            encodedChar = np.append(encodedChar,flat[selChar + max_match - 1])
+            encodedTuple.append((max_match_go_back,max_match))
+            encodedChar.append(encodedChar,flat[selChar + max_match - 1])
             
             
 
-        sbPointer+= 1 +max_match
+        sbPointer= sbPointer+ 1 +max_match
+    
         
 
    
 
-    print("Prova", encodedTuple, encodedChar)
-    print("ArrayBin",encodedTuple.tolist(), encodedChar.tolist())
+    print("Prova", encodedTuple, encodedChar)   
     np.save("encodedTuple", encodedTuple) 
-    np.save("encodedChar", encodedChar)
-    a = encodedTuple.tolist()
-    b = encodedChar.tolist()
-    c= (a,b)
+    np.save("encodedChar", encodedChar)  
     print("File compresso in : Copressed.txt")
     output = open("Compressed.txt","w+")
-    output.write(str(c))
     imgSize = open('imgSize.txt', "w")
     imgSize.write(str(row) + '\n')  # write row dimension
     imgSize.write(str(col) + '\n')  # write col dimension
     imgSize.write(str(ch) + '\n')  # write col dimension
+    output.close()
+    imgSize.close()
     cv2.waitKey(0)
     cv2.destroyAllWindows()
+# fase di decodifica
 # fase di decodifica
 def lz77Decompressor():
     imsize = open("imgSize.txt", "r")
     row = int(imsize.readline())
     col = int(imsize.readline())
-    ch = int(imsize.readline())
+    ch = int (imsize.readline())
     tot = row * col * ch
 
     # carico Tuple e caratteri
@@ -92,7 +97,7 @@ def lz77Decompressor():
     tupleI= 0
     charJ = 0
         #array di decodifica
-    decodeArray = np.array([])
+    decodeArray = []
     while tupleI < encodedTuples.size:
         pBack =  encodedTuples[tupleI]
         charact = encodedChars[charJ]
@@ -102,22 +107,23 @@ def lz77Decompressor():
         tupleI+=2
         # se non vi è nessun prefisso codificato
         if(pBack == 0):
-            decodeArray = np.append(decodeArray,charact)
+            decodeArray.append(charact)
         else:
-            longT = decodeArray.size
+            longT = len(decodeArray)
             for j in range(lunghezza):
                 distance = int(longT - pBack + j)
-            if(distance<decodeArray.size):
-                decodeArray = np.append(decodeArray,decodeArray[distance])
-                if decodeArray.size < tot:
-                    decodeArray = np.append(decodeArray, distance)
+            if(distance<len(decodeArray)):
+                decodeArray.append(distance)
+                if len(decodeArray) < tot:
+                    decodeArray.append( distance)
 
   
-    for i in range(decodeArray.size, tot):
-        decodeArray = np.append(decodeArray, i)
+    for i in range(len(decodeArray), tot):
+        decodeArray.append( i)
     decodeArray = np.reshape(decodeArray,(row,col,ch))
-    cv2.imshow("output", decodeArray)
-    cv2.imwrite("output.png", decodeArray)
-    print("FinalSize",decodeArray.size)
+    imageArray = np.asarray(decodeArray)
+    
+    cv2.imwrite("output.png", imageArray)
+    print("FinalSize",len(imageArray))
     cv2.waitKey(0)
     cv2.destroyAllWindows()
